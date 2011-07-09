efe79914332d4af2f911784424133420ad4adc63
Jaymassena <jay@ariseditions.com>
2011-07-09 06:36:23 +0000

    Added constants

--- src/com/proxibase/aircandi/candi/utils/CandiConstants.java
+++ src/com/proxibase/aircandi/candi/utils/CandiConstants.java
@@ -2,17 +2,21 @@ package com.proxibase.aircandi.candi.utils;
 
 public interface CandiConstants {
 
-	public final int		CANDI_VIEW_WIDTH				= 250;
-	public final int		CANDI_VIEW_SPACING				= 20;
-	public final int		CANDI_VIEW_HEIGHT				= 300;
-	public final int		CANDI_VIEW_BODY_HEIGHT			= 250;
-	public final int		CANDI_VIEW_TITLE_HEIGHT			= 50;
-	public final int		CANDI_VIEW_REFLECTION_HEIGHT	= 125;
-	public final int		CANDI_TITLEBAR_HEIGHT			= 63;
-	public final int		CANDI_VIEW_FONT_SIZE			= 24;
-	public final int		SWIPE_MAX_OFF_PATH				= 500;
+	public static final int	CANDI_VIEW_WIDTH				= 250;
+	public static final int	CANDI_VIEW_SPACING				= 20;
+	public static final int	CANDI_VIEW_HEIGHT				= 300;
+	public static final int	CANDI_VIEW_BODY_HEIGHT			= 250;
+	public static final int	CANDI_VIEW_TITLE_HEIGHT			= 50;
+	public static final int	CANDI_VIEW_REFLECTION_HEIGHT	= 125;
+	public static final int	CANDI_TITLEBAR_HEIGHT			= 63;
+	public static final int	CANDI_VIEW_FONT_SIZE			= 24;
+	public static final int	SWIPE_MAX_OFF_PATH				= 500;
 	public static final int	LAYER_GENERAL					= 0;
 	public static final int	LAYER_ZONES						= 1;
 	public static final int	LAYER_CANDI						= 2;
+	public static final int	DIALOG_INSTALL_ID				= 1;
+	public static final int	DIALOG_WIFIENABLE_ID			= 2;
+	public static final int	VERTEX_INDEX_X					= 0;
+	public static final int	VERTEX_INDEX_Y					= 1;
 
 }
