13257d32ffbc1050b7331c39a6bd5d746b31d5f1
Jay Massena <jay@3meters.com>
2012-07-12 22:27:07 -0700

    To support service error object.

---
+++ src/com/proxibase/service/objects/ServiceError.java
@@ -0,0 +1,14 @@
+package com.proxibase.service.objects;
+
+
+/**
+ * @author Jayma
+ */
+public class ServiceError {
+
+	public String	name;
+	public Number	code;
+	public String	message;
+
+	public ServiceError() {}
+}
