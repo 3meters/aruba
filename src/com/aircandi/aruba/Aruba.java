417031cde412d225d4d448428982d5d71e73de26
Jay Massena <jay@3meters.com>
2014-06-01 09:15:08 -0700

    - updates to get it working again.

--- src/com/aircandi/aruba/Aruba.java
+++ src/com/aircandi/aruba/Aruba.java
@@ -3,6 +3,7 @@ package com.aircandi.aruba;
 import com.aircandi.Aircandi;
 import com.aircandi.components.DispatchManager;
 import com.aircandi.components.StringManager;
+import com.aircandi.utilities.Reporting;
 import com.google.tagmanager.TagManager.RefreshMode;
 
 public class Aruba extends Aircandi {
@@ -16,6 +17,11 @@ public class Aruba extends Aircandi {
 	protected void initializeInstance() {
 		/* Must have this so activity rerouting works. */
 		Aircandi.applicationContext = getApplicationContext();
+		
+		if (!DEBUG) {
+			Reporting.startCrashReporting(this);
+		}
+		
 		super.initializeInstance();
 
 		/* Inject configuration */
